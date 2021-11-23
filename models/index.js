075df1f97ce7763e8d766e4b6d177ae104b9a1fa
herojia <hodalaplus@gmail.com>
2021-11-23 10:16:53 +0800

    Create user model

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-'use strict'
+"use strict"
 
-const fs = require('fs')
-const path = require('path')
-const Sequelize = require('sequelize')
+const fs = require("fs")
+const path = require("path")
+const Sequelize = require("sequelize")
 const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const env = process.env.NODE_ENV || "development"
+const config = require(__dirname + "/../config/config.json")[env]
 const db = {}
 
 // 資料庫連線
@@ -17,18 +17,17 @@ if (config.use_env_variable) {
 }
 
 // 動態引入其他 models
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file))
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
   })
 
 // 設定 Models 之間的關聯
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db)
   }
