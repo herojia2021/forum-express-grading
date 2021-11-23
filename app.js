2837b7494316bfa9a513651aa0dcd50bd9ed54c5
herojia <hodalaplus@gmail.com>
2021-11-23 10:45:15 +0800

    Install express-session connect-flash Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const handlebars = require("express-handlebars") // 引入 handlebars
 const db = require("./models") // 引入資料庫
+const flash = require("connect-flash")
+const session = require("express-session")
 const app = express()
 const port = 3000
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars") // 設定使用 Handlebars 做為樣板引擎
 
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash("success_messages")
+  res.locals.error_messages = req.flash("error_messages")
+  next()
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-app.use(express.urlencoded({ extended: true }))
 
 require("./routes")(app)
 
