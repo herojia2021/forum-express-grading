dcb7b53ae0ceca5acb88dc9d19279c9e02a82115
herojia <hodalaplus@gmail.com>
2021-11-24 04:35:19 +0800

    [Assignment Preparation] ensureAuthenticated & getUser

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const helpers = require("../_helpers")
 const restController = require("../controllers/restController.js")
 const adminController = require("../controllers/adminController.js")
 const userController = require("../controllers/userController.js")
@@ -6,15 +7,15 @@ const upload = multer({ dest: "temp/" })
 const fs = require("fs")
 
 const authenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect("/signin")
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (req.user.isAdmin) {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) {
       return next()
     }
     return res.redirect("/")
