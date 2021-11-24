c1eb48803ef4c152c2bcbfdedba186510d7e9496
herojia <hodalaplus@gmail.com>
2021-11-25 02:54:45 +0800

    Add favorite/unfavorite feature

--- routes/index.js
+++ routes/index.js
@@ -81,4 +81,8 @@ module.exports = (app, passport) => {
   app.post("/comments", authenticated, commentController.postComment)
 
   app.delete("/comments/:id", authenticatedAdmin, commentController.deleteComment)
+
+  app.post("/favorite/:restaurantId", authenticated, userController.addFavorite)
+
+  app.delete("/favorite/:restaurantId", authenticated, userController.removeFavorite)
 }
