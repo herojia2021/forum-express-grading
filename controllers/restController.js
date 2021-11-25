e38b44c5500f272422f998323d395aa6f836f5c1
herojia <hodalaplus@gmail.com>
2021-11-25 10:36:06 +0800

    Add viewCounts

--- controllers/restController.js
+++ controllers/restController.js
@@ -59,12 +59,14 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [Category, { model: User, as: "FavoritedUsers" }, { model: User, as: "LikedUsers" }, { model: Comment, include: [User] }],
     }).then((restaurant) => {
-      const isFavorited = restaurant.FavoritedUsers.map((d) => d.id).includes(req.user.id)
-      const isLiked = restaurant.LikedUsers.map((d) => d.id).includes(req.user.id)
-      return res.render("restaurant", {
-        restaurant: restaurant.toJSON(),
-        isFavorited: isFavorited,
-        isLiked: isLiked,
+      return restaurant.increment("viewCounts", { by: 1 }).then((restaurant) => {
+        const isFavorited = restaurant.FavoritedUsers.map((d) => d.id).includes(req.user.id)
+        const isLiked = restaurant.LikedUsers.map((d) => d.id).includes(req.user.id)
+        return res.render("restaurant", {
+          restaurant: restaurant.toJSON(),
+          isFavorited: isFavorited,
+          isLiked: isLiked,
+        })
       })
     })
   },
